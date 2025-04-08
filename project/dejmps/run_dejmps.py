fe6617e7290c7ec6dc69cc384c5e9782c7a1ef1f
Dragos <dragosileana25@gmail.com>
2025-04-08 12:42:49 +0200

    Implement fail safety

--- project/dejmps/run_dejmps.py
+++ project/dejmps/run_dejmps.py
@@ -1,6 +1,6 @@
 import subprocess
 import numpy as np
-import re
+import pandas as pd
 from time import perf_counter
 
 
@@ -8,13 +8,12 @@ def main():
     # NOTE: To simulate a protocol, this python file needs to be executed
     # inside the folder of the simulated protocol, with venv activated.
 
-    NUM_SAMPLES = 4
-    GRID_SIZE = 3
+    GRID_SIZE = 3  # Set the number of values in the interval [0, 1] for each parameter
+    NUM_SAMPLES = 100  # Set number of samples for each parameter configuration
+    MAX_ATTEMPTS = 2  # Maximum number of attempts for generating a single sample
 
     # # Change directory into the folder of the simulated protocol
     # subprocess.run('ls -lahF', shell=True, cwd='/home/cs4090/CS4090/project/dejmps')
-
-    # Run netqasm simulation and get the results
 
     def parse(bytestring: bytes):
         """Parses simulation outputs."""
@@ -25,33 +24,64 @@ def main():
         m_alice, m_bob, fidelity = s
         return int(m_alice), int(m_bob), float(fidelity)
 
-    results = []
-
-    t0 = perf_counter()
-
+    # Commands to change the gate and channel fidelities in network.yaml
     CMD_GATE_FID = 'sed -i "s/^    gate_fidelity: .*$/    gate_fidelity: {}/g" network.yaml'
     CMD_CHANNEL_FID = 'sed -i "s/^    fidelity: .*$/    fidelity: {}/g" network.yaml'
 
-    for gate_fidelity in np.linspace(0, 1, GRID_SIZE):
-        # Set gate fidelity
-        _ = subprocess.check_output(CMD_GATE_FID.format(gate_fidelity), shell=True)
-        for epr_fidelity in np.linspace(0, 1, GRID_SIZE):
-            # Set EPR channel fidelity
-            _ = subprocess.check_output(CMD_CHANNEL_FID.format(gate_fidelity), shell=True)
+    results = []
+    t0 = perf_counter()
+    num_errors = 0  # Keep track of the number of errors raised by netqasm simulation
 
-            for sample_idx in range(NUM_SAMPLES):
-                output = subprocess.check_output('netqasm simulate', shell=True)
-                m_alice, m_bob, fidelity = parse(output)
-                is_success = 1 - (m_alice ^ m_bob)
-                print([gate_fidelity, epr_fidelity, sample_idx, is_success, fidelity])
-                results.append([gate_fidelity, epr_fidelity, sample_idx, is_success, fidelity])
-                # TODO: Implement fail safety
+    try:
+        # for gate_fidelity in np.linspace(0, 1, GRID_SIZE):
+        for gate_fidelity in [1.0]:
+            # Set gate fidelity
+            _ = subprocess.check_output(CMD_GATE_FID.format(gate_fidelity), shell=True)
+            # for epr_fidelity in np.linspace(0, 1, GRID_SIZE):
+            for epr_fidelity in [0.8]:
+                # Set EPR channel fidelity
+                _ = subprocess.check_output(CMD_CHANNEL_FID.format(epr_fidelity), shell=True)
 
-    results = np.array(results)
+                for sample_idx in range(NUM_SAMPLES):
 
-    print(f'Elapsed time: {perf_counter() - t0}')
-    print('Results:')
-    print(results)
+                    flag = False
+                    attempt_num = 0
+                    while flag and attempt_num < MAX_ATTEMPTS:
+                        try:
+                            output = subprocess.check_output('netqasm simulate', shell=True)
+                        except subprocess.CalledProcessError as err:
+                            num_errors += 1
+                            attempt_num += 1
+                            pass
+                        else:
+                            # Signal that sample was successfully generated
+                            flag = True
+                    # If max attempts is reached, terminate
+                    if attempt_num >= MAX_ATTEMPTS:
+                        raise Exception('Maximum number of attempts reached.')
+
+                    output = subprocess.check_output('netqasm simulate', shell=True)
+
+                    m_alice, m_bob, fidelity = parse(output)
+
+                    results.append([gate_fidelity, epr_fidelity, sample_idx, m_alice, m_bob, fidelity])
+                    print(results[-1])
+                    # TODO: Implement fail safety (after each param config, trying multiple times etc.)
+    finally:
+        print('Saving ...')
+        # Create DataFrame of simulation results and save to CSV file
+        cols = ['Gate fidelity', 'EPR channel fidelity', 'Sample index', 'M_Alice', 'M_Bob', 'Fidelity']
+        results = pd.DataFrame(
+            results,
+            columns=cols,
+        )
+        results.to_csv('./out.csv', mode='a')
+        # NOTE: to read DataFrame from CSV file, do: `pd.read_csv('./store.pkl')`
+
+        print(f'Elapsed time: {perf_counter() - t0}')
+        print(f'No. Errors: {num_errors}')
+        print('Results:')
+        print(results)
 
 
 if __name__ == '__main__':
