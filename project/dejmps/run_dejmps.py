72e7322fd7615f53a254c750bedad39a01326d86
Dragos <dragosileana25@gmail.com>
2025-04-08 13:27:38 +0200

    Refactor run_dejmps.py

--- project/dejmps/run_dejmps.py
+++ project/dejmps/run_dejmps.py
@@ -8,9 +8,11 @@ def main():
     # NOTE: To simulate a protocol, this python file needs to be executed
     # inside the folder of the simulated protocol, with venv activated.
 
-    GRID_SIZE = 3  # Set the number of values in the interval [0, 1] for each parameter
+    # Parameter sweeps
+    GATE_FID_SWEEP = [1.0]  # np.linspace(0, 1, 25)
+    EPR_FID_SWEEP = [0.8]  # np.linspace(0, 1, 25)
     NUM_SAMPLES = 100  # Set number of samples for each parameter configuration
-    MAX_ATTEMPTS = 2  # Maximum number of attempts for generating a single sample
+    MAX_ATTEMPTS = 3  # Maximum number of attempts for generating a single sample
 
     # # Change directory into the folder of the simulated protocol
     # subprocess.run('ls -lahF', shell=True, cwd='/home/cs4090/CS4090/project/dejmps')
@@ -33,26 +35,28 @@ def main():
     num_errors = 0  # Keep track of the number of errors raised by netqasm simulation
 
     try:
-        # for gate_fidelity in np.linspace(0, 1, GRID_SIZE):
-        for gate_fidelity in [1.0]:
+        for gate_fidelity in GATE_FID_SWEEP:
             # Set gate fidelity
             _ = subprocess.check_output(CMD_GATE_FID.format(gate_fidelity), shell=True)
-            # for epr_fidelity in np.linspace(0, 1, GRID_SIZE):
-            for epr_fidelity in [0.8]:
+            for epr_fidelity in EPR_FID_SWEEP:
                 # Set EPR channel fidelity
                 _ = subprocess.check_output(CMD_CHANNEL_FID.format(epr_fidelity), shell=True)
 
                 for sample_idx in range(NUM_SAMPLES):
-
+                    # Attempt multiple times to generate a single sample
+                    output = None
                     flag = False
                     attempt_num = 0
-                    while flag and attempt_num < MAX_ATTEMPTS:
+                    while not flag and attempt_num < MAX_ATTEMPTS:
                         try:
                             output = subprocess.check_output('netqasm simulate', shell=True)
-                        except subprocess.CalledProcessError as err:
+                        except subprocess.SubprocessError | TimeoutError as err:
                             num_errors += 1
                             attempt_num += 1
                             pass
+                        except Exception as other_err:
+                            print(f'Other errors: {other_err}')
+                            raise Exception(f'Other errors: {other_err}')
                         else:
                             # Signal that sample was successfully generated
                             flag = True
@@ -60,14 +64,11 @@ def main():
                     if attempt_num >= MAX_ATTEMPTS:
                         raise Exception('Maximum number of attempts reached.')
 
-                    output = subprocess.check_output('netqasm simulate', shell=True)
-
                     m_alice, m_bob, fidelity = parse(output)
-
                     results.append([gate_fidelity, epr_fidelity, sample_idx, m_alice, m_bob, fidelity])
                     print(results[-1])
-                    # TODO: Implement fail safety (after each param config, trying multiple times etc.)
     finally:
+        # Save simulation results
         print('Saving ...')
         # Create DataFrame of simulation results and save to CSV file
         cols = ['Gate fidelity', 'EPR channel fidelity', 'Sample index', 'M_Alice', 'M_Bob', 'Fidelity']
