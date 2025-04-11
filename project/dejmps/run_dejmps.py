14582648332379cf7e72c0688b8afacc7c010b26
Dragos <dragosileana25@gmail.com>
2025-04-11 19:29:40 +0200

    Run simulation

--- project/dejmps/run_dejmps.py
+++ project/dejmps/run_dejmps.py
@@ -16,8 +16,8 @@ def main():
     # inside the folder of the simulated protocol, with venv activated.
 
     # Parameter sweeps
-    GATE_FID_SWEEP = [1.0]  # [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
-    EPR_FID_SWEEP = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    GATE_FID_SWEEP = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    EPR_FID_SWEEP = [1.0]
     NUM_SAMPLES = 100  # Set number of samples for each parameter configuration
     MAX_ATTEMPTS = 5  # Maximum number of attempts for generating a single sample
 
